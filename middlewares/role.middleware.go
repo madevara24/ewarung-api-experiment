8a5902cb0afb4cec29e7bb29063ecf2eb5c47d77
madevara24 <adityadevara91@gmail.com>
2021-02-10 14:27:00 +0700

    Feat | Make middleware to check shop owner and cashier role

--- middlewares/role.middleware.go
+++ middlewares/role.middleware.go
@@ -28,3 +28,43 @@ func CheckAdmin(next echo.HandlerFunc) echo.HandlerFunc {
 		return nil
 	}
 }
+
+func CheckShopOwner(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := strings.Replace(c.Request().Header.Get("Authorization"), "Bearer ", "", -1)
+		claims, verified := jwt.ExtractClaims(token)
+		if verified && claims["role"] == "SHOP_OWNER" {
+			return next(c)
+		}
+
+		var res models.Response
+
+		res.Status = "Error"
+		res.Message = "Only shop owners can access this endpoint"
+		res.Data = nil
+
+		c.Error(c.JSON(http.StatusUnauthorized, res))
+
+		return nil
+	}
+}
+
+func CheckCashier(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := strings.Replace(c.Request().Header.Get("Authorization"), "Bearer ", "", -1)
+		claims, verified := jwt.ExtractClaims(token)
+		if verified && claims["role"] == "CASHIER" {
+			return next(c)
+		}
+
+		var res models.Response
+
+		res.Status = "Error"
+		res.Message = "Only cashiers can access this endpoint"
+		res.Data = nil
+
+		c.Error(c.JSON(http.StatusUnauthorized, res))
+
+		return nil
+	}
+}
