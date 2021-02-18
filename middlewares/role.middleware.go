2badd51e33b08f13a3ea9f55dfa57d1c2afe232d
madevara24 <adityadevara91@gmail.com>
2021-02-18 14:29:50 +0700

    Refactor | Refactor the jwt implementation in middleware

--- middlewares/role.middleware.go
+++ middlewares/role.middleware.go
@@ -4,22 +4,19 @@ import (
 	"ewarung-api-experiment/jwt"
 	"ewarung-api-experiment/models"
 	"net/http"
-	"strings"
 
 	"github.com/labstack/echo"
 )
 
 func CheckAdmin(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		token := strings.Replace(c.Request().Header.Get("Authorization"), "Bearer ", "", -1)
-		claims, verified := jwt.ExtractClaims(token)
+		claims, verified := jwt.ExtractClaims(c.Request().Header.Get("Authorization"))
 		if verified && claims["role"] == "ADMIN" {
 			return next(c)
 		}
 
 		var res models.Response
 
-		res.Status = "Error"
 		res.Message = "Only admin can access this endpoint"
 		res.Data = nil
 
@@ -31,15 +28,13 @@ func CheckAdmin(next echo.HandlerFunc) echo.HandlerFunc {
 
 func CheckShopOwner(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		token := strings.Replace(c.Request().Header.Get("Authorization"), "Bearer ", "", -1)
-		claims, verified := jwt.ExtractClaims(token)
+		claims, verified := jwt.ExtractClaims(c.Request().Header.Get("Authorization"))
 		if verified && claims["role"] == "SHOP_OWNER" {
 			return next(c)
 		}
 
 		var res models.Response
 
-		res.Status = "Error"
 		res.Message = "Only shop owners can access this endpoint"
 		res.Data = nil
 
@@ -51,15 +46,13 @@ func CheckShopOwner(next echo.HandlerFunc) echo.HandlerFunc {
 
 func CheckCashier(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		token := strings.Replace(c.Request().Header.Get("Authorization"), "Bearer ", "", -1)
-		claims, verified := jwt.ExtractClaims(token)
+		claims, verified := jwt.ExtractClaims(c.Request().Header.Get("Authorization"))
 		if verified && claims["role"] == "CASHIER" {
 			return next(c)
 		}
 
 		var res models.Response
 
-		res.Status = "Error"
 		res.Message = "Only cashiers can access this endpoint"
 		res.Data = nil
 
